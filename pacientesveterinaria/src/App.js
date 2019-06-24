c86f2e2f5e3ec473fd3a3d2a1340774cea426dc3
Silvio Salinas <ssalinas.silvio@gmail.com>
2019-06-23 22:31:23 -0600

    Se crea primer componente de Header y se manda a llamar del componente padre App, enviando como props un titulo

--- pacientesveterinaria/src/App.js
+++ pacientesveterinaria/src/App.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React,{Component} from 'react';
 import './bootstrap.min.css';
+import  Header from './components/Header';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+  state = {  }
+  render() { 
+    return ( 
+      <div className="container">
+        <Header titulo='Administrador Pacientes Veterinaria'></Header>
+      </div>
+     );
+  }
 }
-
+ 
 export default App;
---
+++ pacientesveterinaria/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = ({titulo}) => (
+<header>
+    <h1 className="text-center">
+        {titulo}
+    </h1>
+</header>
+);
+ 
+export default Header;
